33bb4dde112382147ed41836a2cfacd7722069e8
Kavin Kumar <40456399+mrkksparrow@users.noreply.github.com>
2023-12-28 12:24:57 +0530

    Update bpftool_installer.sh

--- bpftool_installer.sh
+++ bpftool_installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt-get install -y clang
-sudo apt-get install -y llvm
-sudo apt-get install -y libelf-dev
-sudo apt-get install -y libc6-dev-i386
-sudo apt-get install -y gcc-multilib
-sudo apt install -y libbpf-dev
-sudo apt-get install -y libcap-dev
+#sudo apt-get install -y clang
+#sudo apt-get install -y llvm
+#sudo apt-get install -y libelf-dev
+#sudo apt-get install -y libc6-dev-i386
+#sudo apt-get install -y gcc-multilib
+#sudo apt install -y libbpf-dev
+#sudo apt-get install -y libcap-dev
 
 # Go to src/cc directory
 cd src/cc
